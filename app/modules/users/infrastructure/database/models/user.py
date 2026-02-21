8fcb2da99b3ee7a08666c69d1670eed8cd4ec81f
Belyashik2K <lovelybelyashik@gmail.com>
2026-02-21 11:20:33 +0300

    refactor: remove last_seen_at column from users table and update related model

--- app/modules/users/infrastructure/database/models/user.py
+++ app/modules/users/infrastructure/database/models/user.py
@@ -1,12 +1,10 @@
 from datetime import datetime
 from typing import (
     TYPE_CHECKING,
-    Optional,
 )
 
 from sqlalchemy import (
     Enum,
-    func,
 )
 from sqlalchemy.orm import (
     Mapped,
@@ -20,11 +18,9 @@ from app.core.infrastructure.database.mixins import (
     TimestampMixin,
 )
 from app.core.shared.enums import UserRoleEnum
-from app.core.shared.utils import get_current_dt
 
 if TYPE_CHECKING:
     from app.modules.auth.infrastructure.database.models import (
-        InstallationModel,
         RefreshSessionModel,
     )
 
@@ -37,18 +33,7 @@ class UserModel(PKUUIDMixin, TimestampMixin, BaseModel):
     role: Mapped[UserRoleEnum] = mapped_column(
         Enum(UserRoleEnum, name="user_role_enum")
     )
-    last_seen_at: Mapped[datetime] = mapped_column(
-        default=get_current_dt,
-        server_default=func.now(),
-        index=True,
-    )
 
-    installation: Mapped[Optional["InstallationModel"]] = relationship(
-        # Only for guest users, for registered users it will be None
-        "InstallationModel",
-        back_populates="user",
-        lazy="joined",
-    )
     refresh_sessions: Mapped[list["RefreshSessionModel"]] = relationship(
         "RefreshSessionModel",
         back_populates="user",
