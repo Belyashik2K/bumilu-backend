73f58f7831c9606b0ec36eb1806e81f32faeca13
Belyashik2K <lovelybelyashik@gmail.com>
2026-02-21 18:48:30 +0300

    feat: implement SQLAlchemyAuthSessionRepository for managing auth sessions

--- app/modules/auth/application/interfaces/repositories/auth_session.py
+++ app/modules/auth/application/interfaces/repositories/auth_session.py
@@ -6,12 +6,11 @@ from abc import (
 from app.core.application.interfaces.repositories import IBaseRepository
 from app.core.shared.domain.value_objects.id import (
     DeviceIdVO,
-    SessionIdVO,
 )
 from app.modules.auth.domain.models.auth_session import AuthSession
 
 
-class IAuthSessionRepository(IBaseRepository[AuthSession, SessionIdVO], ABC):
+class IAuthSessionRepository(IBaseRepository[AuthSession], ABC):
     @abstractmethod
     async def revoke_active_for_device(
         self,
