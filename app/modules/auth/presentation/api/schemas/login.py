c46c19e9b99f088bb9570868ad8274bd44fe2246
Belyashik2K <lovelybelyashik@gmail.com>
2026-02-23 19:19:46 +0300

    refactor: compose device information in headers schema and refactor login endpoints to use headers

--- app/modules/auth/presentation/api/schemas/login.py
+++ app/modules/auth/presentation/api/schemas/login.py
@@ -53,23 +53,6 @@ class VerifyEmailCodeAtLoginRequestSchema(BaseModel):
         description="Verification code sent to the user's email.",
         examples=[VERIFICATION_CODE_EXAMPLE],
     )
-    device_id: UUID7 = Field(
-        ...,
-        description="Unique identifier for the device.",
-        examples=[DEVICE_ID_EXAMPLE],
-    )
-    device_platform: DevicePlatformEnum = Field(
-        ...,
-        description="Platform of the device.",
-        examples=[DevicePlatformEnum.ANDROID],
-    )
-    device_name: str | None = Field(
-        None,
-        description="Name of the device.",
-        examples=["Pixel 9 Pro (Android 14)"],
-    )
-    app_version: str = Field(..., description="Version of the app.", examples=["1.0.0"])
-    # TODO: Compose it in HEADERS
 
 
 class VerifyEmailCodeAtLoginResponseSchema(SuccessfulLoginSchema): ...
