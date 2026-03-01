177b8c132dfa9c391c8a3c0079b9c7600d04eadf
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-01 10:45:46 +0300

    feat: add FavouriteModel

---
+++ app/modules/favourites/infrastructure/database/models/favourite.py
@@ -0,0 +1,38 @@
+from uuid import UUID
+
+from sqlalchemy import (
+    UUID as _UUID,
+)
+from sqlalchemy import (
+    Enum,
+    Index,
+    UniqueConstraint,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+)
+
+from app.core.infrastructure.database import BaseModel
+from app.core.infrastructure.database.mixins import CreatedAtMixin
+from app.modules.favourites.shared.enums import FavouriteEntityTypeEnum
+
+
+class FavouriteModel(CreatedAtMixin, BaseModel):
+    __tablename__ = "favourites"
+
+    user_id: Mapped[UUID] = mapped_column(_UUID, index=True)
+    entity_type: Mapped[FavouriteEntityTypeEnum] = mapped_column(
+        Enum(FavouriteEntityTypeEnum, name="favourite_entity_type_enum"),
+        index=True,
+    )
+    entity_id: Mapped[UUID] = mapped_column(_UUID, index=True)
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "entity_type", "entity_id"),
+        Index(
+            "idx_favourites_entity",
+            "entity_type",
+            "entity_id",
+        ),
+    )
