00371e0d6ca59adf6b9346879240d93211e57512
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-01 10:34:47 +0300

    feat: implement GetAllFavouritesByUser use case and related DTOs

---
+++ app/modules/favourites/application/use_cases/get_all_by_user/dtos.py
@@ -0,0 +1,18 @@
+from dataclasses import (
+    dataclass,
+    field,
+)
+from uuid import UUID
+
+from app.modules.favourites.application.use_cases.common_dtos import FavouriteItemDTO
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class GetAllFavouritesByUserInputDTO:
+    actor_id: UUID
+    user_id: UUID
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class GetAllFavouritesByUserOutputDTO:
+    items: list[FavouriteItemDTO] = field(default_factory=list)
