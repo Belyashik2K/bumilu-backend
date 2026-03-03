46272ba4cbd13fca418a4045aa363ee7c10f471d
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-03 18:14:43 +0300

    fix: add responses for endpoint + add user_id in get all favourites

--- app/modules/favourites/presentation/api/schemas/get.py
+++ app/modules/favourites/presentation/api/schemas/get.py
@@ -11,12 +11,10 @@ from app.modules.users.presentation.api.schemas.common import USER_ID_EXAMPLE
 
 
 class GetAllFavouritesByUserResponseSchema(BaseModel):
-    user_id: UUID7 = (
-        Field(
-            ...,
-            description="The unique identifier of the user whose favourite items are being retrieved.",
-            examples=[USER_ID_EXAMPLE],
-        ),
+    user_id: UUID7 = Field(
+        ...,
+        description="The unique identifier of the user whose favourite items are being retrieved.",
+        examples=[USER_ID_EXAMPLE],
     )
     items: list[FavouriteItemInfoSchema] = Field(
         ...,
