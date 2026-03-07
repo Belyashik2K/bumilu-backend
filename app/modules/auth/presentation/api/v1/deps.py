af52e1759c1c99218284909e25447782937a729f
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-07 18:24:05 +0300

    feat: implement admin principal dependency and enhance authorization checks

--- app/modules/auth/presentation/api/v1/deps.py
+++ app/modules/auth/presentation/api/v1/deps.py
@@ -1,5 +1,8 @@
 from app.core.presentation.custom_request import CustomRequest
-from app.core.shared.exceptions.application.base import ApplicationUnauthorizedException
+from app.core.shared.exceptions.application.base import (
+    ApplicationForbiddenException,
+    ApplicationUnauthorizedException,
+)
 from app.modules.auth.shared.context import Principal
 
 
@@ -7,3 +10,10 @@ def get_principal(request: CustomRequest) -> Principal:
     if not request.state.principal:
         raise ApplicationUnauthorizedException(message="Unauthorized")
     return request.state.principal
+
+
+def get_admin_principal(request: CustomRequest) -> Principal:
+    principal = get_principal(request)
+    if not principal.is_admin():
+        raise ApplicationForbiddenException(message="Know and fight for your rights.")
+    return principal
