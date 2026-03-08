7ae8bc2756f1e7b865030cbec21dd55af0c86b11
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-08 12:41:24 +0300

    refactor: migrate delete review use case to command

--- app/modules/reviews/application/commands/delete/handler.py
+++ app/modules/reviews/application/commands/delete/handler.py
@@ -1,45 +1,36 @@
 import logging
 
-from app.core.application.use_cases.base import IBaseUseCase
+from app.core.application.commands import ICommandHandler
 from app.core.shared.domain.value_objects.id import (
     ReviewIdVO,
     UserIdVO,
 )
 from app.core.shared.utils import prepare_extras
 from app.modules.reviews.application.commands.delete import (
-    DeleteReviewInputDTO,
-    DeleteReviewOutputDTO,
+    DeleteReviewCommand,
 )
 from app.modules.reviews.application.interfaces.repositories.review import (
     IReviewRepository,
 )
 from app.modules.reviews.application.shared.exceptions import (
-    ReviewNotFound,
     ReviewOwnershipViolation,
 )
 
 logger = logging.getLogger(__name__)
 
 
-class DeleteReviewUseCase(
-    IBaseUseCase[
-        DeleteReviewInputDTO,
-        DeleteReviewOutputDTO,
-    ]
-):
+class DeleteReviewCommandHandler(ICommandHandler[DeleteReviewCommand]):
     def __init__(self, review_repository: IReviewRepository) -> None:
         self._review_repository = review_repository
 
-    async def execute(self, input_data: DeleteReviewInputDTO) -> DeleteReviewOutputDTO:
-        review_id = ReviewIdVO.from_uuid(input_data.review_id)
+    async def handle(self, command: DeleteReviewCommand) -> None:
+        review_id = ReviewIdVO.from_uuid(command.review_id)
 
-        review = await self._review_repository.get_by_id(
-            review_id
-        )  # TODO: idempotent delete
+        review = await self._review_repository.get_by_id(review_id)
         if not review:
-            raise ReviewNotFound(review_id=review_id)
+            return None
 
-        actor_id = UserIdVO.from_uuid(input_data.actor_id)
+        actor_id = UserIdVO.from_uuid(command.actor_id)
         if review.author_id != actor_id:
             logger.warning(
                 "review_delete_forbidden",
@@ -53,4 +44,4 @@ class DeleteReviewUseCase(
 
         await self._review_repository.delete_by_id(review_id)
 
-        return DeleteReviewOutputDTO()
+        return None
