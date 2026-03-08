37e9acb9e2b94bebb55046ae7f0766c7b7845f0c
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-08 09:33:56 +0300

    feat: start migration to CQRS

---
+++ app/core/application/commands/base.py
@@ -0,0 +1,32 @@
+from abc import (
+    ABC,
+    abstractmethod,
+)
+from dataclasses import dataclass
+from typing import Generic
+
+from app.core.application.base_handler import (
+    BaseHandler,
+    CommandDTO,
+    ResultDTO,
+)
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class EmptyCommand: ...
+
+
+class ICommandHandler(Generic[CommandDTO], BaseHandler, ABC):
+    async def __call__(self, command: CommandDTO) -> None:
+        return await self._run_with_observability(command, self.handle)
+
+    @abstractmethod
+    async def handle(self, command: CommandDTO) -> None: ...
+
+
+class ICommandHandlerWithResult(Generic[CommandDTO, ResultDTO], BaseHandler, ABC):
+    async def __call__(self, command: CommandDTO) -> ResultDTO:
+        return await self._run_with_observability(command, self.handle)
+
+    @abstractmethod
+    async def handle(self, command: CommandDTO) -> ResultDTO: ...
---
+++ app/core/application/queries/base.py
@@ -0,0 +1,19 @@
+from abc import (
+    ABC,
+    abstractmethod,
+)
+from typing import Generic
+
+from app.core.application.base_handler import (
+    BaseHandler,
+    QueryDTO,
+    ResultDTO,
+)
+
+
+class IQueryHandler(Generic[QueryDTO, ResultDTO], BaseHandler, ABC):
+    async def __call__(self, query: QueryDTO) -> ResultDTO:
+        return await self._run_with_observability(query, self.handle)
+
+    @abstractmethod
+    async def handle(self, query: QueryDTO) -> ResultDTO: ...
