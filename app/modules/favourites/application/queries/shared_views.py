8e58d84de0142aa6da52dc433c0fe21a791d2b6a
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-09 21:17:10 +0300

    feat: implement favourites retrieval functionality with pagination support

--- app/modules/favourites/application/queries/shared_views.py
+++ app/modules/favourites/application/queries/shared_views.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+from datetime import datetime
 from uuid import UUID
 
 from app.modules.favourites.shared.enums import FavouriteEntityTypeEnum
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
-class FavouriteItemDTO:
+class FavouriteEntityView:
     entity_type: FavouriteEntityTypeEnum
     entity_id: UUID
+    created_at: datetime
