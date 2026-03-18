dd7651fba3840e9a295f65f58f0d44bf6baa4527
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-18 20:57:18 +0300

    refactor: move info about fav entity in "entity" key

--- app/modules/favourites/application/queries/get_all_by_user/view.py
+++ app/modules/favourites/application/queries/get_all_by_user/view.py
@@ -5,17 +5,17 @@ from dataclasses import (
 from uuid import UUID
 
 from app.core.shared.application.queries.pagination import OffsetPagination
-from app.modules.favourites.application.queries.shared_views import FavouriteEntityView
+from app.modules.favourites.application.queries.shared_views import FavouriteView
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
 class FavouritesPage:
-    items: list[FavouriteEntityView]
+    items: list[FavouriteView]
     total: int
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
 class PaginatedFavouritesView:
     user_id: UUID
-    favourites: list[FavouriteEntityView] = field(default_factory=list)
+    favourites: list[FavouriteView] = field(default_factory=list)
     pagination: OffsetPagination
--- app/modules/favourites/application/queries/shared_views.py
+++ app/modules/favourites/application/queries/shared_views.py
@@ -6,7 +6,12 @@ from app.modules.favourites.shared.enums import FavouriteEntityTypeEnum
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
-class FavouriteEntityView:
-    entity_type: FavouriteEntityTypeEnum
-    entity_id: UUID
+class FavouriteEntityInfoView:
+    id: UUID
+    type: FavouriteEntityTypeEnum
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class FavouriteView:
+    entity: FavouriteEntityInfoView
     created_at: datetime
