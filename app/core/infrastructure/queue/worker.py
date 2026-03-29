a6711a80848b7a365049a2b4ab788af100162379
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-29 19:45:15 +0300

    refactor: di -> ioc folder for better clarity

--- app/core/infrastructure/queue/worker.py
+++ app/core/infrastructure/queue/worker.py
@@ -1,17 +1,16 @@
 from dishka import make_async_container
 from dishka.integrations.taskiq import setup_dishka
 
-import app.core.infrastructure.database.models_registry
-from app.core.di import (
+from app.core.infrastructure.queue.broker import broker
+from app.core.ioc import (
     ConfigProvider,
     DatabaseProvider,
 )
-from app.core.infrastructure.queue.broker import broker
-from app.modules.auth.di import AuthEmailProvider
-from app.modules.chat.di import (
+from app.modules.auth.ioc import AuthEmailProvider
+from app.modules.chat.ioc import (
     CHAT_PROVIDERS,
 )
-from app.modules.users.di import UserProvider
+from app.modules.users.ioc import UserProvider
 
 container = make_async_container(
     ConfigProvider(),
@@ -22,5 +21,4 @@ container = make_async_container(
 )
 setup_dishka(broker=broker, container=container)
 
-import app.modules.auth.infrastructure.queue.tasks
 import app.modules.chat.infrastructure.queue.tasks  # noqa: F401
