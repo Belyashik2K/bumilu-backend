3344dd11c23e9400c782568280db209d3ec5c4e5
Belyashik2K <lovelybelyashik@gmail.com>
2026-03-31 12:03:51 +0300

    feat: add timezone support and new PlaceCardView structure, finalize list method in reader

--- app/core/utils/datetime.py
+++ app/core/utils/datetime.py
@@ -3,7 +3,15 @@ from datetime import (
     datetime,
 )
 
+import pytz
+
 
 def get_current_dt() -> datetime:
     dt = datetime.now(tz=UTC)
+    return dt.replace(microsecond=0)
+
+
+def get_current_dt_in_timezone(timezone: str) -> datetime:
+    tz = pytz.timezone(timezone)
+    dt = datetime.now(tz=tz)
     return dt.replace(microsecond=0)
