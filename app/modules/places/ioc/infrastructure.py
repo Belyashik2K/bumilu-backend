eb581b5814c395d69ab9389598b1b71a2c4f3779
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-05 21:20:29 +0300

    feat: add PlacesCommandHandlersProvider and PlacesPersistenceProvider for command handling and persistence

--- app/modules/places/ioc/infrastructure.py
+++ app/modules/places/ioc/infrastructure.py
@@ -1,31 +1,6 @@
 from dishka import (
     Provider,
-    Scope,
-    provide,
-)
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.modules.places.application.interfaces.readers.place import (
-    IPlaceReader,
-)
-from app.modules.places.application.queries.categories.shared.readers.place_category import (
-    IPlaceCategoryReader,
-)
-from app.modules.places.infrastructure.database.readers.place import (
-    SQLAlchemyPlaceReader,
-)
-from app.modules.places.infrastructure.database.readers.place_category import (
-    SQLAlchemyPlaceCategoryReader,
 )
 
 
-class PlacesInfrastructureProvider(Provider):
-    @provide(scope=Scope.REQUEST, provides=IPlaceReader)
-    async def place_reader(self, session: AsyncSession) -> IPlaceReader:
-        return SQLAlchemyPlaceReader(session=session)
-
-    @provide(scope=Scope.REQUEST, provides=IPlaceCategoryReader)
-    async def place_category_reader(
-        self, session: AsyncSession
-    ) -> SQLAlchemyPlaceCategoryReader:
-        return SQLAlchemyPlaceCategoryReader(session=session)
+class PlacesInfrastructureProvider(Provider): ...
