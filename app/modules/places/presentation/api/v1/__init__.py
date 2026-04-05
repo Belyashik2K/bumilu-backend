4789335555c7022bf46c979f9f20d3aab155bdf6
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-05 22:01:44 +0300

    fix: routers registration

--- app/modules/places/presentation/api/v1/__init__.py
+++ app/modules/places/presentation/api/v1/__init__.py
@@ -5,6 +5,6 @@ from .categories.user_router import user_place_categories_router
 from .places.router import user_places_router
 
 places_router = APIRouter()
-places_router.include_router(user_places_router)
 places_router.include_router(user_place_categories_router)
 places_router.include_router(admin_place_categories_router)
+places_router.include_router(user_places_router)
