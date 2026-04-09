c4988ca8caea324b0e18f7ecfb11a4757f90806f
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-09 13:19:37 +0300

    feat: implement GetAdminPlaceCategoryTranslationQueryHandler

--- app/modules/places/application/queries/categories/admin/get/view.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from app.modules.places.application.queries.categories.shared.models.place_category import (
-    PlaceCategoryReadModel,
-)
-
-
-@dataclass(frozen=True, slots=True, kw_only=True)
-class AdminPlaceCategoryView(PlaceCategoryReadModel): ...
--- app/modules/places/application/queries/categories/shared/readers/place_category_translation.py
+++ app/modules/places/application/queries/categories/shared/readers/place_category_translation.py
@@ -4,12 +4,20 @@ from abc import (
 )
 from uuid import UUID
 
+from app.core.enums import LanguageEnum
 from app.modules.places.application.queries.categories.shared.models.place_category import (
     PlaceCategoryTranslationReadModel,
 )
 
 
 class IPlaceCategoryTranslationReader(ABC):
+    @abstractmethod
+    async def get_by_category_id_and_language_code(
+        self,
+        category_id: UUID,
+        language_code: LanguageEnum,
+    ) -> PlaceCategoryTranslationReadModel | None: ...
+
     @abstractmethod
     async def list_by_category_id(
         self,
