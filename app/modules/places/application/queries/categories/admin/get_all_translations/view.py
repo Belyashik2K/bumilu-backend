9bf29237f9515f55c9e69444c45f213168f826f0
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-09 14:08:16 +0300

    feat: implement pagination for admin place category translations and impl query endpoints

---
+++ app/modules/places/application/queries/categories/admin/get_all_translations/view.py
@@ -0,0 +1,15 @@
+from dataclasses import (
+    dataclass,
+    field,
+)
+
+from app.core.application.queries.pagination import OffsetPagination
+from app.modules.places.application.queries.categories.shared.models.place_category import (
+    PlaceCategoryTranslationReadModel,
+)
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class PaginatedAdminPlaceCategoryTranslationsView:
+    translations: list[PlaceCategoryTranslationReadModel] = field(default_factory=list)
+    pagination: OffsetPagination
--- app/modules/places/infrastructure/database/readers/place_category_translation.py
+++ app/modules/places/infrastructure/database/readers/place_category_translation.py
@@ -1,8 +1,12 @@
 from uuid import UUID
 
-from sqlalchemy import select
+from sqlalchemy import (
+    func,
+    select,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.application.queries.pagination import PageReadModel
 from app.core.enums import LanguageEnum
 from app.modules.places.application.queries.categories.shared.models.place_category import (
     PlaceCategoryTranslationReadModel,
@@ -40,16 +44,46 @@ class SQLAlchemyPlaceCategoryTranslationReader(IPlaceCategoryTranslationReader):
     async def list_by_category_id(
         self,
         category_id: UUID,
-    ) -> list[PlaceCategoryTranslationReadModel]:
-        stmt = select(PlaceCategoryTranslationModel).where(
+        limit: int,
+        offset: int,
+    ) -> PageReadModel[PlaceCategoryTranslationReadModel]:
+        count_stmt = (
+            select(func.count(func.distinct(PlaceCategoryTranslationModel.id)))
+            .select_from(PlaceCategoryTranslationModel)
+            .where(PlaceCategoryTranslationModel.category_id == category_id)
+        )
+
+        items_stmt = select(PlaceCategoryTranslationModel).where(
             PlaceCategoryTranslationModel.category_id == category_id,
         )
+
+        total_subquery = count_stmt.scalar_subquery()
+
+        stmt = (
+            items_stmt.add_columns(total_subquery.label("total_count"))
+            .limit(limit)
+            .offset(offset)
+        )
+
         result = await self._session.execute(stmt)
-        translations = result.scalars().all()
-        return [
-            PlaceCategoryTranslationReadModel(
-                language_code=translation.language_code,
-                name=translation.name,
-            )
-            for translation in translations
-        ]  # TODO: move to mappers
+        rows = result.unique().all()
+
+        if not rows:
+            total = await self._session.scalar(count_stmt)
+            return PageReadModel(total=total)
+
+        translations: list[PlaceCategoryTranslationModel] = [
+            row.PlaceCategoryTranslationModel for row in rows
+        ]
+        total = rows[0].total_count
+
+        return PageReadModel(
+            items=[
+                PlaceCategoryTranslationReadModel(
+                    language_code=translation.language_code,
+                    name=translation.name,
+                )
+                for translation in translations
+            ],
+            total=total,
+        )
