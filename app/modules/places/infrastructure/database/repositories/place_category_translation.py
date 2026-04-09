01e8c64c8fff6e9afbf1df55e49b91eb2286bd94
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-09 11:32:29 +0300

    feat: implement funcs in repos

--- app/modules/places/infrastructure/database/repositories/place_category_translation.py
+++ app/modules/places/infrastructure/database/repositories/place_category_translation.py
@@ -1,9 +1,14 @@
+from sqlalchemy import (
+    delete,
+    select,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.domain.value_objects.id import (
     PlaceCategoryIdVO,
     PlaceCategoryTranslationIdVO,
 )
+from app.core.enums import LanguageEnum
 from app.core.infrastructure.database import SQLAlchemyBaseRepository
 from app.modules.places.application.interfaces.repositories.place_category_translation import (
     IPlaceCategoryTranslationRepository,
@@ -48,3 +53,22 @@ class SQLAlchemyPlaceCategoryTranslationRepository(
             language_code=model.language_code,
             name=PlaceCategoryNameVO(model.name),
         )
+
+    async def get_by_category_id_and_language_code(
+        self, category_id: PlaceCategoryIdVO, language_code: LanguageEnum
+    ) -> PlaceCategoryTranslation | None:
+        stmt = select(PlaceCategoryTranslationModel).where(
+            PlaceCategoryTranslationModel.category_id == category_id.value,
+            PlaceCategoryTranslationModel.language_code == language_code,
+        )
+        result = await self.session.execute(stmt)
+        model = result.scalar_one_or_none()
+        return self._to_entity(model) if model else None
+
+    # TODO: catchers
+    async def delete_by_id(self, translation_id: PlaceCategoryTranslationIdVO) -> None:
+        stmt = delete(PlaceCategoryTranslationModel).where(
+            PlaceCategoryTranslationModel.id == translation_id.value,
+        )
+        await self.session.execute(stmt)
+        await self.session.flush()
