bc1a086cb65b34c017cee5d32e99d6e9ee60484b
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-09 09:39:31 +0300

    feat: implement UpdatePlaceCategoryCommandHandler

--- app/modules/places/application/interfaces/repositories/place_category.py
+++ app/modules/places/application/interfaces/repositories/place_category.py
@@ -1,7 +1,13 @@
-from abc import ABC
+from abc import (
+    ABC,
+    abstractmethod,
+)
 
 from app.core.application.interfaces.repositories import IBaseRepository
 from app.modules.places.domain.categories.models.category.model import PlaceCategory
+from app.modules.places.domain.categories.value_objects.slug import PlaceCategorySlugVO
 
 
-class IPlaceCategoryRepository(IBaseRepository[PlaceCategory], ABC): ...
+class IPlaceCategoryRepository(IBaseRepository[PlaceCategory], ABC):
+    @abstractmethod
+    async def get_by_slug(self, slug: PlaceCategorySlugVO) -> PlaceCategory | None: ...
