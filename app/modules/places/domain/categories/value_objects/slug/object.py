3bbe251f554ba2f2196e89e05bb1b8f96b487e89
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-10 15:36:11 +0300

    feat: add "-" in allowed chars for place category slug

--- app/modules/places/domain/categories/value_objects/slug/object.py
+++ app/modules/places/domain/categories/value_objects/slug/object.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from string import ascii_lowercase
 
 from app.modules.places.domain.categories.value_objects.slug.exceptions import (
     InvalidSlug,
 )
+
+ALLOWED_CHARACTERS = ascii_lowercase + "-"
 
 
 @dataclass(frozen=True, slots=True)
@@ -13,7 +16,7 @@ class PlaceCategorySlugVO:
         if not self.value:
             return False
 
-        return self.value.isalpha()
+        return all(char in ALLOWED_CHARACTERS for char in self.value)
 
     def __post_init__(self) -> None:
         if not self._is_valid_slug():
