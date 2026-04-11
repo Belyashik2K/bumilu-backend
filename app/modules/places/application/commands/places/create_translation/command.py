6c7b4eb01cd8dd218511d7bc0fcd0612f6b965f9
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-11 10:00:33 +0300

    feat: implement UpdatePlaceTranslationCommandHandler

--- app/modules/places/application/commands/places/create_translation/command.py
+++ app/modules/places/application/commands/places/create_translation/command.py
@@ -1,23 +1,18 @@
 from dataclasses import (
     dataclass,
-    field,
 )
 from uuid import UUID
 
-from app.core.constants import (
-    UNSET,
-    UnsetType,
-)
 from app.core.enums import LanguageEnum
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
 class PlaceTranslationData:
     language_code: LanguageEnum
-    title: str | UnsetType = field(default=UNSET)
-    description: str | UnsetType = field(default=UNSET)
-    short_description: str | UnsetType = field(default=UNSET)
-    display_address: str | UnsetType = field(default=UNSET)
+    title: str
+    description: str
+    short_description: str
+    display_address: str
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
