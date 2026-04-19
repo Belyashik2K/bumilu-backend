f8ca0cba582eaf906e98214fc996be7b5b14c95a
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-19 15:44:12 +0300

    feat: add pydantic schemas for responses

--- app/modules/routes/presentation/api/schemas/card.py
+++ app/modules/routes/presentation/api/schemas/card.py
@@ -14,26 +14,6 @@ from app.modules.routes.presentation.api.schemas.examples import (
 )
 from app.modules.routes.shared.enums.route_status import RouteStatusEnum
 
-# @dataclass(frozen=True, slots=True, kw_only=True)
-# class BaseRouteCardReadModel:
-#     id: UUID
-#     total_places: int
-#
-#
-# @dataclass(frozen=True, slots=True, kw_only=True)
-# class RouteCardReadModel(BaseRouteCardReadModel):
-#     title: str
-#     short_description: str
-#     m_to_start_place: int | None = field(default=None)
-#
-#
-# @dataclass(frozen=True, slots=True, kw_only=True)
-# class AdminRouteCardReadModel(BaseRouteCardReadModel):
-#     title: str | None = field(default=None)
-#     status: RouteStatusEnum
-#     created_at: datetime
-#     updated_at: datetime
-
 
 class BaseRouteCardSchema(BaseModel):
     id: UUID7 = Field(
