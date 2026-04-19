3603c3581320f43a1acb3fcac4b8c5d9517ab15c
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-19 19:56:23 +0300

    feat: implement GetStaffMembersListQueryHandler

--- app/modules/staff/application/queries/get/handler.py
+++ app/modules/staff/application/queries/get/handler.py
@@ -4,11 +4,13 @@ from app.modules.staff.application.interfaces.readers.staff_member import (
     IStaffMemberReader,
 )
 from app.modules.staff.application.queries.get.query import GetStaffMemberQuery
-from app.modules.staff.application.queries.shared.views import StaffMemberInfoView
+from app.modules.staff.application.queries.shared.models.staff_member_info import (
+    StaffMemberInfoReadModel,
+)
 
 
 class GetStaffMemberQueryHandler(
-    IQueryHandler[GetStaffMemberQuery, StaffMemberInfoView]
+    IQueryHandler[GetStaffMemberQuery, StaffMemberInfoReadModel]
 ):
     def __init__(
         self,
@@ -16,7 +18,7 @@ class GetStaffMemberQueryHandler(
     ) -> None:
         self.staff_member_reader = staff_member_reader
 
-    async def handle(self, query: GetStaffMemberQuery) -> StaffMemberInfoView:
+    async def handle(self, query: GetStaffMemberQuery) -> StaffMemberInfoReadModel:
         staff_member = await self.staff_member_reader.get_by_id(query.staff_member_id)
         if not staff_member:
             raise StaffMemberNotFound(query.staff_member_id)  # type: ignore
