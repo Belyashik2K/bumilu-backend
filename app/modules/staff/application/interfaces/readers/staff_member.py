3a00ae4e51112999f28fbdcb9e20512585acadb3
Belyashik2K <lovelybelyashik@gmail.com>
2026-04-19 19:56:23 +0300

    feat: implement GetStaffMembersListQueryHandler

--- app/modules/staff/application/interfaces/readers/staff_member.py
+++ app/modules/staff/application/interfaces/readers/staff_member.py
@@ -4,9 +4,27 @@ from abc import (
 )
 from uuid import UUID
 
-from app.modules.staff.application.queries.shared.views import StaffMemberInfoView
+from app.core.application.queries.pagination import PageReadModel
+from app.modules.staff.application.queries.shared.models.staff_member_info import (
+    StaffMemberFullInfoReadModel,
+    StaffMemberInfoReadModel,
+)
 
 
 class IStaffMemberReader(ABC):
     @abstractmethod
-    async def get_by_id(self, staff_member_id: UUID) -> StaffMemberInfoView | None: ...
+    async def get_by_id(
+        self, staff_member_id: UUID
+    ) -> StaffMemberInfoReadModel | None: ...
+
+    @abstractmethod
+    async def get_full_info_by_id(
+        self, staff_member_id: UUID
+    ) -> StaffMemberFullInfoReadModel | None: ...
+
+    @abstractmethod
+    async def get_all(
+        self,
+        limit: int,
+        offset: int,
+    ) -> PageReadModel[StaffMemberFullInfoReadModel]: ...
